b8430298760062398623441cb887c2efd031ed26
Sinkumen <sinkumen@gmail.com>
2025-03-12 15:19:30 +0300

    Remove unused middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -34,33 +34,4 @@ const protectRoute = async (req: Request, res: Response, next: any) => {
   }
 };
 
-const optionalAuth = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    let token: string;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-
-      const decoded = jwt.verify(
-        token,
-        process.env.JWT_SECRET || ""
-      ) as JwtPayload;
-      req.user = await User.findByPk(decoded.id);
-    }
-    next();
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(401)
-      .json({ error: "Not authorized to access this route" });
-  }
-};
-
-export { protectRoute, optionalAuth };
+export { protectRoute };
